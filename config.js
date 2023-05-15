67a49b4ab75c80b2e87c1f8a7cc40d9acc298d85
Encoder <124882893+Anvir0001@users.noreply.github.com>
2023-05-15 12:32:01 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,52 @@
 console.log('Hello')
 
 console.log('To do something')
+
+{
+    "cmd": {
+      "errorMessage": "An error occured: Contact The Developer",
+      "dev": {
+        "emit": {
+          "success": "Successfully emitted event **({EVENT})**.",
+          "error": "Sorry, I couldn't emit **{EVENT}**. Please try again!"
+        }
+      },
+      "manage": {
+        "lang": {
+          "update": "Successfully updated the language to **{LANGUAGE}**."
+        }
+      },
+      "info": {
+        "user": {
+          "DescJoin": "On <t:{joinTime}:D>, {username} joined as the **{addSuffix}** member of this guild",
+          "name": "Name",
+          "badges": "Badges",
+          "booster": "Booster",
+          "toproles": "Top Roles",
+          "created": "Created",
+          "joined": "Joined"
+        },
+        "bot": {
+          "ping": "Ping",
+          "uptime": "Uptime",
+          "users": "Users",
+          "channels": "Channels",
+          "guilds": "Guilds",
+          "largestguilds": "Largest Guilds"
+        }
+      }
+    },
+    "events": {},
+    "comps": {
+      "button": {
+        "thread": {
+          "invalidParent": "An error occured: Cannot find parent channel",
+          "invalidMessage": "An error occured: Cannot find starter message",
+          "threadLocked": "Thread locked by **{userTag}**",
+          "notAuthorized": "You are not authorized to perform this action."
+        }
+      }
+    },
+    "other": {}
+  }
+  
